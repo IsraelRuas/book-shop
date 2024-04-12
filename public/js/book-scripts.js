96552fd10585fdfa5f69f74ffc344a494904f975
Israel Ruas <ruas.it@outlook.com>
2024-04-13 06:11:53 +1000

    Book Management done

--- public/js/book-scripts.js
+++ public/js/book-scripts.js
@@ -1,16 +1,20 @@
 /*
 
-############ PUPULATING THE QUANTITY SELECT
+############ PUPULATING THE QUANTITY SELECT BY THE BOOK NAME AND ITS QUANTITY
 
 */
-
-function populateBookQuantity(quantityInStock) {
+//Function get the values by params.
+function populateBookQuantity(quantityInStock, bookName) {
     const x = quantityInStock
-    const select = document.getElementById('bookQuantity')
-    for (let i = 2; i <= x; i++) {
-        const option = document.createElement('option')
-        option.value = i
-        option.text = 'Quantity: ' + i
-        select.appendChild(option)
+    const select = document.getElementById(bookName)
+    //Condition to check if the options are populated
+    if (typeof select.options != 'undefined' && select.options.length < 2) {
+        //Looping to populate the select until the maximum of Stock quantity
+        for (let i = 2; i <= x; i++) {
+            const option = document.createElement('option')
+            option.value = i
+            option.text = 'Quantity: ' + i
+            select.appendChild(option)
+        }
     }
 }
